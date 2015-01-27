883c9f7b6a65372896f2370e0c10d6c528b890d6
n_h <h.10x64@gmail.com>
2015-01-27 22:44:43 +0900

    oscillatorTypeプロパティで正常に値が反映されないバグを修正

--- dist/snd.all.in.one.js
+++ dist/snd.all.in.one.js
@@ -3659,6 +3659,9 @@ snd.CLASS_DEF.push(function() {
                 },
                 set: function(val) {
                     this._status.oscillatorType = val;
+                    if (this._source) {
+                        this._source.type = val;
+                    }
 
                     this._periodicWave = null;
                     this._status.periodicWave = null;
