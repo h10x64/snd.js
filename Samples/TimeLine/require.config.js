341893bfb0df2600801946d76c48320144525528
n_h <h.10x64@gmail.com>
2015-08-01 13:06:24 +0900

    Bower用のファイル追加

---
+++ Samples/TimeLine/require.config.js
@@ -0,0 +1,32 @@
+/*
+ * require.jsのコンフィグファイルです。<br/>
+ */
+var require = {
+    paths: {
+        'snd': '../../dist/snd',
+        'snd.util': '../../dist/snd.util',
+        'snd.AudioDataManager': '../../dist/class/required/snd.AudioDataManager',
+        'snd.AudioMaster': '../../dist/class/required/snd.AudioMaster',
+        'snd.AudioUnit': '../../dist/class/required/snd.AudioUnit',
+        'snd.Exception': '../../dist/class/required/snd.Exception',
+        'snd.Source': '../../dist/class/required/snd.Source',
+        'snd.Analyser': '../../dist/class/optional/snd.Analyser',
+        'snd.BiquadFilter': '../../dist/class/optional/snd.BiquadFilter',
+        'snd.BufferSource': '../../dist/class/optional/snd.BufferSource',
+        'snd.Convolver': '../../dist/class/optional/snd.Convolver',
+        'snd.Delay': '../../dist/class/optional/snd.Delay',
+        'snd.DynamicsCompressor': '../../dist/class/optional/snd.DynamicsCompressor',
+        'snd.Gain': '../../dist/class/optional/snd.Gain',
+        'snd.MediaElementAudioSource': '../../dist/class/optional/snd.MediaElementAudioSource',
+        'snd.MediaStreamAudioSource': '../../dist/class/optional/snd.MediaStreamAudioSource',
+        'snd.OscillatorSource': '../../dist/class/optional/snd.OscillatorSource',
+        'snd.ScriptProcessor': '../../dist/class/optional/snd.ScriptProcessor',
+        'snd.Synth': '../../dist/class/optional/snd.Synth',
+        'snd.WaveShaper': '../../dist/class/optional/snd.WaveShaper',
+        'snd.Noise': '../../dist/class/optional/snd.Noise',
+        'snd.VinylNoise': '../../dist/class/optional/snd.VinylNoise',
+        'snd.MIDI': '../../dist/class/optional/MIDI/snd.MIDI',
+        'snd.MIDI.util': '../../dist/class/optional/MIDI/snd.MIDI.util'
+    }
+};
+
